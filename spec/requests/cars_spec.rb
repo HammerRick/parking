a42c15574c5bbe2ee9a031f167fb6f48377799a1
Emerick94 <lucas.emerick94@gmail.com>
2020-07-04 13:22:21 -0300

    add requests specs

--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -12,16 +12,16 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/cars", type: :request do
+RSpec.describe '/cars', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Car. As you add validations to Car, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { plate: 'AAB-0000' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { plate: '2AB-000' }
   }
 
   # This should return the minimal set of values that should be in the headers
@@ -32,92 +32,92 @@ RSpec.describe "/cars", type: :request do
     {}
   }
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Car.create! valid_attributes
       get cars_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       car = Car.create! valid_attributes
       get car_url(car), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Car" do
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Car' do
         expect {
           post cars_url,
                params: { car: valid_attributes }, headers: valid_headers, as: :json
         }.to change(Car, :count).by(1)
       end
 
-      it "renders a JSON response with the new car" do
+      it 'renders a JSON response with the new car' do
         post cars_url,
              params: { car: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Car" do
+    context 'with invalid parameters' do
+      it 'does not create a new Car' do
         expect {
           post cars_url,
                params: { car: invalid_attributes }, as: :json
         }.to change(Car, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new car" do
+      it 'renders a JSON response with errors for the new car' do
         post cars_url,
              params: { car: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq('application/json; charset=utf-8')
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { plate: 'AAB-0001' }
       }
 
-      it "updates the requested car" do
+      it 'updates the requested car' do
         car = Car.create! valid_attributes
         patch car_url(car),
-              params: { car: invalid_attributes }, headers: valid_headers, as: :json
+              params: { car: new_attributes }, headers: valid_headers, as: :json
         car.reload
-        skip("Add assertions for updated state")
+        expect(car.plate).to eq('AAB-0001')
       end
 
-      it "renders a JSON response with the car" do
+      it 'renders a JSON response with the car' do
         car = Car.create! valid_attributes
         patch car_url(car),
-              params: { car: invalid_attributes }, headers: valid_headers, as: :json
+              params: { car: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq('application/json; charset=utf-8')
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the car" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the car' do
         car = Car.create! valid_attributes
         patch car_url(car),
               params: { car: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to eq("application/json")
+        expect(response.content_type).to eq('application/json; charset=utf-8')
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested car" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested car' do
       car = Car.create! valid_attributes
       expect {
         delete car_url(car), headers: valid_headers, as: :json
