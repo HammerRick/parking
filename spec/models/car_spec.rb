86835e9a0a4d3deb5fba8320a28f3ac29b21ea24
Emerick94 <lucas.emerick94@gmail.com>
2020-07-04 16:21:36 -0300

    spec improvements

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,58 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  before(:each) do
-    @car = create(:car)
-  end
+  let(:car) { create :car }
 
   context 'plate validations' do
     it 'is valid with a valid plate' do
-      expect(@car).to be_valid
+      expect(car).to be_valid
     end
 
     it 'is invalid with a valid but alredy used plate' do
-      duplicated_car = @car.dup
+      duplicated_car = car.dup
       expect(duplicated_car).to be_invalid
     end
 
     it 'is invalid without a plate' do
-      @car.plate = nil
-      expect(@car).to be_invalid
+      car.plate = nil
+      expect(car).to be_invalid
     end
 
     it 'is invalid with a blank plate' do
-      @car.plate = ''
-      expect(@car).to be_invalid
+      car.plate = ''
+      expect(car).to be_invalid
     end
 
     it 'is invalid with a plate without numbers' do
-      @car.plate = 'ASA-ADSA'
-      expect(@car).to be_invalid
+      car.plate = 'ASA-ADSA'
+      expect(car).to be_invalid
     end
 
     it 'is invalid with a plate without letters' do
-      @car.plate = '122-3214'
-      expect(@car).to be_invalid
+      car.plate = '122-3214'
+      expect(car).to be_invalid
     end
 
     it 'is invalid with too few letters' do
-      @car.plate = 'AS-3214'
-      expect(@car).to be_invalid
+      car.plate = 'AS-3214'
+      expect(car).to be_invalid
     end
 
     it 'is invalid with too much letters' do
-      @car.plate = 'ASQW-3214'
-      expect(@car).to be_invalid
+      car.plate = 'ASQW-3214'
+      expect(car).to be_invalid
     end
 
     it 'is invalid with too few numbers' do
-      @car.plate = 'ASA-321'
-      expect(@car).to be_invalid
+      car.plate = 'ASA-321'
+      expect(car).to be_invalid
     end
 
     it 'is invalid with too much letters' do
-      @car.plate = 'ASQ-13214'
-      expect(@car).to be_invalid
+      car.plate = 'ASQ-13214'
+      expect(car).to be_invalid
     end
   end
 end
--- spec/models/parking_ticket_spec.rb
+++ spec/models/parking_ticket_spec.rb
@@ -1,20 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ParkingTicket, type: :model do
-  before(:each) do
-    @parking_ticket = create!(:parking_ticket)
-  end
+  let(:parking_ticket) { create :parking_ticket }
 
   context 'relationship validation' do
     it 'is valid with a car' do
-      expect(@parking_ticket.car).not_to be_nil
-      expect(@parking_ticket).to be_valid
+      expect(parking_ticket.car).not_to be_nil
+      expect(parking_ticket).to be_valid
     end
 
     it 'is invalid without a car' do
-      @parking_ticket.car = nil
-      expect(@parking_ticket.car).to be_nil
-      expect(@parking_ticket).to be_valid
+      parking_ticket.car = nil
+      expect(parking_ticket.car).to be_nil
+      expect(parking_ticket).to be_invalid
     end
   end
 end
