2a437f6257d50a10b9ed010158ae9f0fee977163
Emerick94 <lucas.emerick94@gmail.com>
2020-07-05 20:34:02 -0300

    improves spec on post /parking

--- spec/requests/parking_request_spec.rb
+++ spec/requests/parking_request_spec.rb
@@ -2,33 +2,64 @@ require 'rails_helper'
 
 RSpec.describe 'Parkings', type: :request do
 
+  let(:valid_plate) { 'AAB-0000' }
+  let(:invalid_plate) { '2AB-000' }
+
   describe 'post /' do
     context 'valid plate' do
       it 'returns http success' do
-        post '/parking/', params: { plate: 'FAA-1234' }
+        post '/parking/', params: { plate: valid_plate }
 
         expect(response).to have_http_status(:success)
       end
 
       it 'renders a JSON response with the car`s plate number' do
-        post '/parking/', params: { plate: 'FAA-1234' }
-        pt = ParkingTicker.last
+        post '/parking/', params: { plate: valid_plate }
+        parking_ticket= ParkingTicket.last
 
-        expect(JSON.parse(response.body)).to eq({ id: pt.id, message: 'Car with plate number FAA-1234 sucefully parked.' })
+        expect(JSON.parse(response.body)).to eq(
+          {
+            'id' => parking_ticket.id,
+            'message' => "Car with plate number #{valid_plate} sucefully parked."
+          }
+        )
       end
     end
 
     context 'invalid plate' do
       it 'returns http bad_request' do
-        post '/parking/'
+        post '/parking/', params: { plate: invalid_plate }
 
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'renders a JSON response with errors for the parking attempt' do
-        post '/parking/'
+        post '/parking/', params: { plate: invalid_plate }
 
-        expect(JSON.parse(response.body)).to eq({ plate: 'FAA-1234' })
+        expect(JSON.parse(response.body)).to eq(
+          {
+            'error' => 'invalid',
+            'plate' => ['must follow the style: AAA-0000']
+          }
+        )
+      end
+    end
+
+    context 'valid plate but car alredy has an active parking_ticket' do
+      let(:car) { create :car, plate: valid_plate}
+      let!(:parking_ticket) { create :parking_ticket, car: car}
+      it 'returns http bad_request' do
+        post '/parking/', params: { plate: valid_plate }
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'renders a JSON response with errors for the parking attempt' do
+        post '/parking/', params: { plate: valid_plate }
+
+        expect(JSON.parse(response.body)).to eq(
+          { 'error' => "This car is currently parked with and older ticket, id: #{parking_ticket.id}." }
+        )
       end
     end
   end
@@ -51,32 +82,32 @@ RSpec.describe 'Parkings', type: :request do
   describe 'PUT /:id/pay' do
     context 'unpaid ticket' do
       it 'returns http success' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/pay"
+        put "/parking/#{parking_ticket.id}/pay"
         expect(response).to have_http_status(:success)
       end
 
       it 'renders a JSON response confirming payment' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/pay"
+        put "/parking/#{parking_ticket.id}/pay"
         expect(JSON.parse(response.body)).to eq({ message: 'Thank you, payment received!' })
       end
     end
 
     context 'paid ticket' do
       it 'returns http bad_request' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/pay"
+        put "/parking/#{parking_ticket.id}/pay"
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'returns http success' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/pay"
+        put "/parking/#{parking_ticket.id}/pay"
         expect(JSON.parse(response.body)).to eq({ message: 'Error, please pay your parking ticket.' })
       end
     end
@@ -85,32 +116,32 @@ RSpec.describe 'Parkings', type: :request do
   describe 'PUT /:id/out' do
     context 'paid ticket' do
       it 'returns http success' do
-        pt = create(:parking_ticket, :paid)
+        parking_ticket= create(:parking_ticket, :paid)
 
-        put "/parking/#{pt.id}/out"
+        put "/parking/#{parking_ticket.id}/out"
         expect(response).to have_http_status(:success)
       end
 
       it 'renders a JSON response confirming end of transaction' do
-        pt = create(:parking_ticket, :paid)
+        parking_ticket= create(:parking_ticket, :paid)
 
-        put "/parking/#{pt.id}/out"
+        put "/parking/#{parking_ticket.id}/out"
         expect(JSON.parse(response.body)).to eq({ message: 'Thank you for you preference, good bye!' })
       end
     end
 
     context 'unpaid ticket' do
       it 'returns http bad_request' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/out"
+        put "/parking/#{parking_ticket.id}/out"
         expect(response).to have_http_status(:bad_request)
       end
 
       it 'renders a JSON response requesting payment' do
-        pt = create(:parking_ticket)
+        parking_ticket= create(:parking_ticket)
 
-        put "/parking/#{pt.id}/out"
+        put "/parking/#{parking_ticket.id}/out"
         expect(JSON.parse(response.body)).to eq({ message: 'Error, please pay your parking ticket.' })
       end
     end
@@ -118,16 +149,16 @@ RSpec.describe 'Parkings', type: :request do
 
   describe 'GET /:plate' do
     it 'returns http success' do
-      pt = create(:parking_ticket, :paid)
-      get "/parking/#{pt.plate}"
+      parking_ticket= create(:parking_ticket, :paid)
+      get "/parking/#{parking_ticket.plate}"
       expect(response).to have_http_status(:success)
     end
 
     it 'renders a JSON response with requested information' do
-      pt = create(:parking_ticket, :paid)
-      get "/parking/#{pt.plate}"
+      parking_ticket= create(:parking_ticket, :paid)
+      get "/parking/#{parking_ticket.plate}"
       expect(JSON.parse(response.body)).to eq([
-        { id: pt.id, time: '25 minutes', paid: true, left: false }
+        { id: parking_ticket.id, time: '25 minutes', paid: true, left: false }
       ])
     end
   end
