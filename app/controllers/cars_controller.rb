41e7623825949d7dd95ffa320b927fc9bb7b801f
Emerick94 <lucas.emerick94@gmail.com>
2020-07-06 03:20:16 -0300

    fix identation

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -39,13 +39,14 @@ class CarsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_car
-      @car = Car.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def car_params
-      params.require(:car).permit(:model, :brand, :color, :plate)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def car_params
+    params.require(:car).permit(:model, :brand, :color, :plate)
+  end
 end
