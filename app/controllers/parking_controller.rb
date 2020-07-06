da4607289f0b9d148e28947684cb62f1fac45945
Emerick94 <lucas.emerick94@gmail.com>
2020-07-05 21:59:26 -0300

    implements /parking/:id/out

--- app/controllers/parking_controller.rb
+++ app/controllers/parking_controller.rb
@@ -35,6 +35,19 @@ class ParkingController < ApplicationController
   end
 
   def out
+    ticket_id = params['id']
+    parking_ticket = ParkingTicket.find_by(id: ticket_id)
+
+    if parking_ticket.nil?
+      render json: { error: "Parking Ticket with id #{ticket_id} not found." }, status: :not_found
+    elsif !parking_ticket.paid
+      render json: { error: 'Please pay this ticket before leaving.' }, status: :bad_request
+    elsif parking_ticket.out_at.nil?
+      parking_ticket.out_at = Time.zone.now if parking_ticket.out_at.nil?
+      render json: { message: 'Thank you for you preference, good bye!' }
+    else
+      render json: { message: "Your car has alredy been returned to you at #{parking_ticket.out_at}" }
+    end
   end
 
   def history
