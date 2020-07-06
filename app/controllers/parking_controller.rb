81d224072e6707914da00e44eb1dc6e7ea473ee5
Emerick94 <lucas.emerick94@gmail.com>
2020-07-05 21:34:48 -0300

    implements /parking/:id/pay

--- app/controllers/parking_controller.rb
+++ app/controllers/parking_controller.rb
@@ -8,6 +8,7 @@ class ParkingController < ApplicationController
 
     if car.new_record?
       render json: { error: 'invalid' }.merge(car.errors.messages), status: :bad_request
+
     elsif car.can_park?
       parking_ticket = car.parking_tickets.create(in_at: Time.zone.now)
       render json: { id: parking_ticket.id, message: "Car with plate number #{car.plate} sucefully parked."},
@@ -20,6 +21,17 @@ class ParkingController < ApplicationController
   end
 
   def pay
+    ticket_id = params['id']
+    parking_ticket = ParkingTicket.find_by(id: ticket_id)
+
+    if parking_ticket.nil?
+      render json: { error: "Parking Ticket with id #{ticket_id} not found." }, status: :not_found
+    elsif parking_ticket.paid
+      render json: { message: 'Thank you, but this ticket has already been paid, new payment not accepted.' }
+    else
+      parking_ticket.paid = true
+      render json: { message: 'Thank you, payment received!' }
+    end
   end
 
   def out
