80720bdec310105746b44bf9b782d01026472396
Emerick94 <lucas.emerick94@gmail.com>
2020-07-06 15:04:55 -0300

    inprove car specs

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -54,6 +54,23 @@ RSpec.describe Car, type: :model do
     end
   end
 
+  describe 'plate normalized after save' do
+    it 'do nothing on a normalized plate' do
+      car = create(:car, plate: 'MMX-8456')
+      expect(car.plate).to eq('MMX-8456')
+    end
+
+    it 'change downcase letters to upcase' do
+      car = create(:car, plate: 'mmx-8456')
+      expect(car.plate).to eq('MMX-8456')
+    end
+
+    it 'add "-" in case there is none' do
+      car = create(:car, plate: 'MMX8456')
+      expect(car.plate).to eq('MMX-8456')
+    end
+  end
+
   describe 'parking_tickets validations' do
     it 'is valid with no parking tickets' do
       expect(car.parking_tickets.count).to eq(0)
@@ -104,6 +121,38 @@ RSpec.describe Car, type: :model do
       expect(car.parking_tickets.count).to eq(1)
       expect(car.parking_tickets.active.count).to eq(1)
       expect(car.can_park?).to be false
+    end
+  end
+
+  describe 'by_plate' do
+    let!(:car) { create :car, plate: 'MMZ-4231'}
+
+    it 'finds car using an exact match' do
+      expect(Car.by_plate('MMZ-4231')).to eq(car)
+    end
+
+    it 'finds car without using "-" on the plate' do
+      expect(Car.by_plate('MMZ4231')).to eq(car)
+    end
+
+    it 'finds car using lowercase letters on the plate' do
+      expect(Car.by_plate('mmz-4231')).to eq(car)
+    end
+
+    it "does't find car using wrong # of letters" do
+      expect(Car.by_plate('mmmz-4231')).to eq(nil)
+    end
+
+    it "does't find car using wrong # of numbers" do
+      expect(Car.by_plate('mmz-42311')).to eq(nil)
+    end
+
+    it "does't find car using empty string" do
+      expect(Car.by_plate('')).to eq(nil)
+    end
+
+    it "does't find car using nil" do
+      expect(Car.by_plate(nil)).to eq(nil)
     end
   end
 end
