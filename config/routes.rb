c1db5f83c7cf664fc34d5cbca24810dff35d2bc5
Emerick94 <lucas.emerick94@gmail.com>
2020-07-06 03:19:37 -0300

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   scope '/parking', controller: 'parking' do
-    get '', to: 'parking#index'
     post '', to: 'parking#in'
     put ':id/pay', to: 'parking#pay'
     put ':id/out', to: 'parking#out'
